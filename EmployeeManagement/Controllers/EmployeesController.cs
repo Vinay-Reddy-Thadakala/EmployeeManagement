47b5bb72577f4f60f63544aa083a35df2d3e86b9
Vinay-Reddy-Thadakala <vinayreddythadakala@gmail.com>
2025-05-24 23:14:37 +0530

    nitial commit: setup ASP.NET Core Web API project

---
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,101 @@
+﻿
+using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using EmployeeManagement.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/employees
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = _context.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+        // GET: api/employees/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if(employee == null)
+            {
+                return NotFound($"Employee with ID {id} not found.");
+            }            
+
+            return Ok(employee);
+        }
+
+        // POST: api/employees
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            _context.Employees.Add(employeeEntity);
+            _context.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+        // PUT: api/employees/{id}
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound($"Employee with ID {id} not found.");
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            _context.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        // DELETE: api/employees/{id}
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = _context.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound($"Employee with ID {id} not found.");
+            }
+
+            _context.Employees.Remove(employee);
+            _context.SaveChanges(); 
+
+            return Ok(employee);
+        }
+    }
+}
